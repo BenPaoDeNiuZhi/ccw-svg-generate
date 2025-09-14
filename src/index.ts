491c53b0ae1d4f1357cbbf410de14e21822efb01
BenPaoDeNiuZhi <3817684776@qq.com>
2025-09-15 03:15:19 +0800

    更新 index.ts
    误触

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,7 @@ export default {
 	async fetch(req, env, ctx) {
 		const url = new URL(req.url);
 		let template:string, params:Array;
-		try{l.
+		try{
 			template=url.searchParams.get('template') || url.searchParams.get('t') || '未传入数据'
 			params=JSON.parse(url.searchParams.get('params') || url.searchParams.get('param') || '[]')
 		}
