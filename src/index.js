edd8debf3173f41c41cc22d2d30839682aab0e19
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2025-09-14 16:16:09 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
- * - Run `npm run dev` in your terminal to start a development server
+ * - Run "npm run dev" in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
+ * - Run "npm run deploy" to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+function generate(pattern,param,req){
+  let dat=pattern
+  console.log(param)
+  const ip=req.headers.get("x-real-ip")
+  for(let i of param){
+    console.log(i)
+    if(i=="ip"){
+      dat = dat.replace("%s",ip)
+    }
+  }
+  console.log(dat)
+  return dat
+}
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(req, env, ctx) {
+    const url = new URL(req.url)
+    return new Response(
+      generate(
+        url.searchParams.get("template"),
+        JSON.parse(url.searchParams.get("param")),
+        req
+      ),{
+        headers:{
+          "Content-type":"image/svg+xml"
+        }
+      }
+    );
+  }
 };
