0cbd8c98cc5fac302f82e11cf3b7dc5c7fc06a0b
BenPaoDeNiuZhi <3817684776@qq.com>
2025-09-16 01:57:32 +0800

    更新 tokenizer.test.ts

--- test/tokenizer.test.ts
+++ test/tokenizer.test.ts
@@ -6,12 +6,13 @@ describe('tokenize',()=>{
         console.log(res)
         expect(res).toMatchObject({type:'func',funcName:'a',args:[]})
     })
-    it('tokenize a function with args: \"a(\"a\",1)\"',()=>{
+    it('tokenize a function with args: \"a(\"a\",1, 1.5)\"',()=>{
         const res = tokenize('a(\"a\",1)')
         console.log(res)
         expect(res).toMatchObject({type:'func',funcName:'a'})
         expect(res.args[0].dat).toBe('a')
         expect(res.args[1].dat).toBe(1)
+        expect(res.args[2].dat).toBe(1.5)
     })
     it('tokenize a string: \"a\"',()=>{
         const res = tokenize('\"a\"')
