19691e324d849d65634d4d6927bffc1f106e0b55
BenPaoDeNiuZhi <3817684776@qq.com>
2025-09-17 00:38:39 +0800

    更新 execute.test.ts

--- test/execute.test.ts
+++ test/execute.test.ts
@@ -10,6 +10,15 @@ it('execute a keyword: ip',()=>{
 it('execute an undefined keyword: xxx',()=>{
     expect(exec(new token_keyword('xxx'),{ip:'127.0.0.1'})).toBe('undefined')
 })
-it('execute concat: concat(ip,\" hello")',()=>{
-    expect(exec(new token_function('concat',[new token_keyword("ip"),new token_string(" hello")]),{ip:'127.0.0.1'})).toBe('127.0.0.1 hello')
+it('execute concat: concat(ip,\" hello\")',()=>{
+    expect(exec(new token_function('concat',[
+        new token_keyword("ip"),
+        new token_string(" hello")
+    ]),{ip:'127.0.0.1'})).toBe('127.0.0.1 hello')
+})
+it('execute if(else): ?("true","1","2")',()=>{
+    expect(exec(new token_function('?',[
+        new token_string("true"),
+        new token_string("1"),
+        new token_string("2")]),{})).toBe('1')
 })
