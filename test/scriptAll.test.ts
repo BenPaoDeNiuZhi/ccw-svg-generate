a7ff0591d4cbe6257cbe2c38a003ea8915b89c0c
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-09-17 20:18:47 +0800

    更新 scriptAll.test.ts

--- test/scriptAll.test.ts
+++ test/scriptAll.test.ts
@@ -1,1 +1,7 @@
-//a
+import { exec } from "../src/execute";
+import { tokenize } from "../src/tokenizer";
+import { describe, it, expect } from 'vitest';
+it("run \"hello world\"",()=>{
+    const t = tokenize("\"hello world\"")
+    expect(exec(t,{})).toEqual("hello world")
+})
