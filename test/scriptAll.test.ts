237287ec508926750cfbba869c22e5bcbcc2d676
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-09-17 20:23:29 +0800

    更新 scriptAll.test.ts

--- test/scriptAll.test.ts
+++ test/scriptAll.test.ts
@@ -1,7 +1,15 @@
 import { exec } from "../src/execute";
 import { tokenize } from "../src/tokenizer";
 import { describe, it, expect } from 'vitest';
-it("run \"hello world\"",()=>{
+it("run str \"hello world\"",()=>{
     const t = tokenize("\"hello world\"")
     expect(exec(t,{})).toEqual("hello world")
+})
+it("run keyword ip",()=>{
+    const t = tokenize("ip")
+    expect(exec(t,{ip:"127.0.0.1"})).toEqual("127.0.0.1")
+})
+it("run if-else ?(\"true\",\"t\",0)",()=>{
+    const t = tokenize("?(\"true\",\"t\",0)")
+    expect(exec(t,{})).toEqual("t")
 })
