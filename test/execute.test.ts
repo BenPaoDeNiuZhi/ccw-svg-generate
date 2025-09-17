d537cf56ec4d0b1eb16acdbfd3a506ce1aa85235
BenPaoDeNiuZhi <3817684776@qq.com>
2025-09-18 00:36:03 +0800

    更新 execute.test.ts

--- test/execute.test.ts
+++ test/execute.test.ts
@@ -1,23 +1,23 @@
 import { exec } from "../src/execute";
 import { token_string, token_keyword, token_function} from "../src/tokenizer";
 import { describe, it, expect } from 'vitest';
-it('execute a string: \"abcd\"',()=>{
-    expect(exec(new token_string('abcd'),{})).toBe('abcd')
+it('execute a string: \"abcd\"',async ()=>{
+    expect(await exec(new token_string('abcd'),{})).toBe('abcd')
 })
-it('execute a keyword: ip',()=>{
-    expect(exec(new token_keyword('ip'),{ip:'127.0.0.1'})).toBe('127.0.0.1')
+it('execute a keyword: ip',async ()=>{
+    expect(await exec(new token_keyword('ip'),{ip:'127.0.0.1'})).toBe('127.0.0.1')
 })
 it('execute an undefined keyword: xxx',()=>{
-    expect(exec(new token_keyword('xxx'),{ip:'127.0.0.1'})).toBe(undefined)
+    expect(await exec(new token_keyword('xxx'),{ip:'127.0.0.1'})).toBe(undefined)
 })
 it('execute concat: concat(ip,\" hello\")',()=>{
-    expect(exec(new token_function('concat',[
+    expect(await exec(new token_function('concat',[
         new token_keyword("ip"),
         new token_string(" hello")
     ]),{ip:'127.0.0.1'})).toBe('127.0.0.1 hello')
 })
 it('execute if(else): ?(true,"1","2")',()=>{
-    expect(exec(new token_function('?',[
+    expect(await exec(new token_function('?',[
         new token_string("true"),
         new token_string("1"),
         new token_string("2")]),{})).toBe('1')
