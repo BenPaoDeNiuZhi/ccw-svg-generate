af52c7f4a2b76d8327cd69ac932709a1ee43ecce
BenPaoDeNiuZhi <3817684776@qq.com>
2025-09-17 23:13:56 +0800

    更新 index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,2 +1,5 @@
-import { fillParams } from "../src/index";
+import { fillParam } from "../src/index";
 import { describe, it, expect } from 'vitest';
+it("fill param [\"bbb\"] to tempate \"aaa%sccc\"",()=>{
+    expect(fillParam("aaa%sccc",["bbb"])).toEqual("aaabbbccc")
+})
