d17224db975522b2ffe0416cee409076284c2fa2
BenPaoDeNiuZhi <3817684776@qq.com>
2025-09-18 00:37:44 +0800

    更新 scriptAll.test.ts

--- test/scriptAll.test.ts
+++ test/scriptAll.test.ts
@@ -1,29 +1,29 @@
 import { exec } from "../src/execute";
 import { tokenize } from "../src/tokenizer";
 import { describe, it, expect } from 'vitest';
-it("run str \"hello world\"",()=>{
+it("run str \"hello world\"",async ()=>{
     const t = tokenize("\"hello world\"")
-    expect(exec(t,{})).toEqual("hello world")
+    expect(await exec(t,{})).toEqual("hello world")
 })
-it("run keyword ip",()=>{
+it("run keyword ip",async ()=>{
     const t = tokenize("ip")
-    expect(exec(t,{ip:"127.0.0.1"})).toEqual("127.0.0.1")
+    expect(await exec(t,{ip:"127.0.0.1"})).toEqual("127.0.0.1")
 })
-it("run if-else ?(true,\"t\",0)",()=>{
+it("run if-else ?(true,\"t\",0)",async ()=>{
     let t = tokenize("?(true,\"t\",0)")
-    expect(exec(t,{})).toEqual("t")
+    expect(await exec(t,{})).toEqual("t")
     t = tokenize("?(false,\"t\",0)")
-    expect(exec(t,{})).toEqual(0)
+    expect(await exec(t,{})).toEqual(0)
     t = tokenize("?(false,\"t\")")
-    expect(exec(t,{})).toEqual(undefined)
+    expect(await exec(t,{})).toEqual(undefined)
 })
-it("run concat cct(\"a\",\"b\", \"c\")",()=>{
+it("run concat cct(\"a\",\"b\", \"c\")",async ()=>{
     const t = tokenize("cct(\"a\",\"b\", \"c\")")
-    expect(exec(t,{})).toEqual("abc")
+    expect(await exec(t,{})).toEqual("abc")
 })
-it("run equal ==(\"a\",\"b\") ==(\"a\",\"a\")",()=>{
+it("run equal ==(\"a\",\"b\") ==(\"a\",\"a\")",async ()=>{
     let t = tokenize("==(\"a\",\"b\")")
-    expect(exec(t,{})).toEqual(false)
+    expect(await exec(t,{})).toEqual(false)
     t = tokenize("==(\"a\",\"a\")")
-    expect(exec(t,{})).toEqual(true)
+    expect(await exec(t,{})).toEqual(true)
 })
